0e2fe9110a516a6b5803e4dc054fcd207d418804
Alexey Losev <reventon399@gmail.com>
2023-08-10 16:45:09 +0500

    Initial Commit

---
+++ MySparTestTask/MySparTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparTestTaskApp.swift
+//  MySparTestTask
+//
+//  Created by Алексей Лосев on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MySparTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
